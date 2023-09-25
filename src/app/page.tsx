f28f820b11e509f31b441fa3bb015b2a516f7cd2
denis.makarov <denis.a.makarov@ringcentral.com>
2023-09-25 23:05:51 +0400

    eslint configuration + basic routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
-export default function MainPage () {
-    return <div>Main page</div>;
+import {redirect} from 'next/navigation';
+
+export default function MainPage() {
+    redirect('overview');
 }
