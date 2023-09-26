70d1269dd6920caccf061ed59c3023d4675c3ab3
denis.makarov <denis.a.makarov@ringcentral.com>
2023-09-26 23:35:09 +0400

    fix antd flash of unstyled ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,8 @@ import {Inter} from 'next/font/google';
 import {Layout, Menu} from 'antd';
 import {AppstoreOutlined, MailOutlined} from '@ant-design/icons';
 import React from 'react';
-import Link from "next/link";
+import Link from 'next/link';
+import RootStyleRegistry from '@fc/modules/shared/root-style-registry/RootStyleRegistry';
 
 const inter = Inter({subsets: ['latin']});
 
@@ -21,31 +22,33 @@ export default function RootLayout({children}: Props) {
     return (
         <html lang="en">
             <body className={inter.className}>
-                <div>
-                    <Layout>
-                        <Menu
-                            mode={'horizontal'}
-                            items={[
-                                {
-                                    label: <Link href="/overview">Overview</Link>,
-                                    key: 'overview',
-                                    icon: <MailOutlined/>,
-                                },
-                                {
-                                    label: <Link href="/desks">Desks</Link>,
-                                    key: 'desks',
-                                    icon: <AppstoreOutlined/>,
-                                },
-                                {
-                                    label: <Link href="/stats">Stats</Link>,
-                                    key: 'stats',
-                                    icon: <AppstoreOutlined/>,
-                                },
-                            ]}
-                        />
-                        <div>{children}</div>
-                    </Layout>
-                </div>
+                <RootStyleRegistry>
+                    <div>
+                        <Layout>
+                            <Menu
+                                mode={'horizontal'}
+                                items={[
+                                    {
+                                        label: <Link href="/overview">Overview</Link>,
+                                        key: 'overview',
+                                        icon: <MailOutlined/>,
+                                    },
+                                    {
+                                        label: <Link href="/desks">Desks</Link>,
+                                        key: 'desks',
+                                        icon: <AppstoreOutlined/>,
+                                    },
+                                    {
+                                        label: <Link href="/stats">Stats</Link>,
+                                        key: 'stats',
+                                        icon: <AppstoreOutlined/>,
+                                    },
+                                ]}
+                            />
+                            <div>{children}</div>
+                        </Layout>
+                    </div>
+                </RootStyleRegistry>
             </body>
         </html>
     );
